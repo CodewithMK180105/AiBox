9146292d1382bf0fcf99ef38a7020bd6e8ec3e8a
CodewithMK180105 <manishkumar180105@gmail.com>
2025-09-11 20:50:55 +0530

    Navbar, Hero Section, Clerk Done.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,11 @@ import Layout from './pages/Layout'
 import Dashboard from './pages/Dashboard'
 import WriteArticle from './pages/WriteArticle'
 import BlogTitles from './pages/BlogTitles'
+import GenerateImages from './pages/GenerateImages'
+import Removebackground from './pages/Removebackground'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
 
 const App = () => {
   return (
@@ -13,7 +18,12 @@ const App = () => {
         <Route path='/ai' element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path='write-articles' element={<WriteArticle />} />
-          <Route path='blog-titles'  element={<BlogTitles />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-background' element={<Removebackground />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='community' element={<Community />} />
         </Route>
       </Routes>
     </>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Hero from "../components/Hero"
+import Navbar from "../components/Navbar"
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Navbar/>
+      <Hero />
     </div>
   )
 }
