23511076ca910324ee11272499b5516ecb579ec2
CodewithMK180105 <manishkumar180105@gmail.com>
2025-09-19 19:15:07 +0530

    Integrated tools, and testimonials sections

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import AiTools from "../components/AiTools"
 import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
+import Testimonial from "../components/Testimonial"
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Hero />
+      <AiTools />
+      <Testimonial />
     </div>
   )
 }
