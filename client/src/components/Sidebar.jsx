79feec3db2005dfbdfb3412626394acd23a812fd
CodewithMK180105 <manishkumar180105@gmail.com>
2025-09-22 09:26:21 +0530

    Sidebar z-index provided

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -51,7 +51,7 @@ const Sidebar = ({sidebar, setSidebar}) => {
     const {signOut, openUserProfile}= useClerk();
 
   return (
-    <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${sidebar? 'translate-x-0':'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
+    <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${sidebar? 'translate-x-0':'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out z-10`}>
       <div className="my-7 w-full">
         <img src={user.imageUrl} alt="User Avatar" className="w-13 rounded-full mx-auto" />
         <h1 className="mt-1 text-center">{user.fullName}</h1>
